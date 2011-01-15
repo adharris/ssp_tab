e860fd43b2bd67db316ec25aa40dee9653769b80
Andrew <adharris@gmail.com>
2011-01-15 18:53:05 -0500

    added css, started basic layouts

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,12 @@ module SspCtab
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    ###Part of spork hack. see http://bit.ly/arY19y
+    if Rails.env.test?
+      initializer :after => :initialize_dependency_mechanism do
+        ActiveSupport::Dependencies.mechanism = :load
+      end
+    end
   end
 end
