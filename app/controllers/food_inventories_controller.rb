eecfd71f8abe70e19823eaeb8c7d70d4bd3c32c6
Andrew <adharris@gmail.com>
2011-02-22 21:00:15 -0500

    added show/create/edit for food inventories

--- app/controllers/food_inventories_controller.rb
+++ app/controllers/food_inventories_controller.rb
@@ -8,4 +8,39 @@ class FoodInventoriesController < ApplicationController
     @title = @program.nil? ? "Food Inventories" : "Food Inventories for #{@program}"
   end
 
+  def show
+    @title = "#{@food_inventory.date} Food Inventory"
+  end
+
+  def new
+    @title = "New Food Inventory"
+  end
+
+  def create
+    if @food_inventory.save
+      flash[:success] = "Food inventory created successfully"
+      redirect_to @food_inventory
+    else
+      @title = "New Food Inventory"
+      render :new
+    end
+  end
+
+  def edit
+    @title = "Editing #{@food_inventory.date} Food Inventory"
+    @program = @food_inventory.program
+  end
+
+  def update
+    if @food_inventory.update_attributes(params[:food_inventory])
+      flash[:success] = "Food inventory updated successfully"
+      redirect_to @food_inventory
+    else
+      @title = "Editing #{@food_inventory.date} Food Inventory"
+      render :edit
+    end
+  end
+      
+
+
 end
