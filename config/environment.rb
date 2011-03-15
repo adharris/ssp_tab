7514686518fa9d872cb8d62d13d1d1c02c49d3a2
Andrew <adharris@gmail.com>
2011-03-14 20:49:47 -0400

    still setting up mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 SspCtab::Application.initialize!
 
-#Setup the host domain
-config.action_mailer.default_url_options = { :host => 'ssp-food.heroku.com' }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ SspCtab::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  #Setup the host domain
+  config.action_mailer.default_url_options = { :host => 'ssp-food.heroku.com' }
 end
