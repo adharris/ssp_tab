ee75a86cb4e646d10c69924ac9082276acece0e0
Andrew <adharris@gmail.com>
2011-03-14 20:46:31 -0400

    set up mailer hostname

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SspCtab::Application.initialize!
+
+#Setup the host domain
+config.action_mailer.default_url_options = { :host => 'ssp-food.heroku.com' }
