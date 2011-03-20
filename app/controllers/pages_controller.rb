8b18c1fa0b1fbbe7e7891c9e6c351cb4abe29e85
Andrew <adharris@gmail.com>
2011-03-20 17:41:34 -0400

    Filter by program on home screen

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,11 @@ class PagesController < ApplicationController
   def home
     @title = "Home"
     @date = params[:month] ? Date.parse(params[:month]) : Date.today
-    @purchases = Purchase.all
-    @inventories = FoodInventory.all
+    @program = Program.find(params[:program]) unless params[:program].blank?
+    @purchases = @program.nil? ? Purchase.all : @program.purchases
+    @inventories = @program.nil? ? FoodInventory.all : @program.food_inventories
+    @items = @purchases + @inventories
+
   end
 
 end
