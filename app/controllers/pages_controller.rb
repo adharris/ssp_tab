2781ae4db72277f8731803fae8f75255f4c81f88
Andrew <adharris@gmail.com>
2011-03-20 18:24:00 -0400

    normal user homepage calendar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,10 @@ class PagesController < ApplicationController
     @title = "Home"
     @date = params[:month] ? Date.parse(params[:month]) : Date.today
     @program = Program.find(params[:program]) unless params[:program].blank?
+    @program = current_user.current_program unless can?(:manage, Program) || current_user.nil?
     @purchases = @program.nil? ? Purchase.all : @program.purchases
     @inventories = @program.nil? ? FoodInventory.all : @program.food_inventories
-    @items = @purchases + @inventories
-
+    @items =  @inventories + @purchases 
   end
 
 end
