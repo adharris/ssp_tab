d3cfd0300da96cf7f971dac88c6f236d2a181b3f
Andrew <adharris@gmail.com>
2011-03-20 16:27:27 -0400

    Added home page calendar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   
   def home
     @title = "Home"
+    @date = params[:month] ? Date.parse(params[:month]) : Date.today
+    @purchases = Purchase.all
+    @inventories = FoodInventory.all
   end
 
 end
