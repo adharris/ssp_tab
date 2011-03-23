ec19e682e78fbba8253727a4225368d673f80475
Andrew <adharris@gmail.com>
2011-03-22 21:33:12 -0400

    fixed default Program:active

--- db/migrate/20110322230417_add_active_to_program.rb
+++ db/migrate/20110322230417_add_active_to_program.rb
@@ -1,6 +1,6 @@
 class AddActiveToProgram < ActiveRecord::Migration
   def self.up
-    add_column :programs, :active, :boolean, :default => 1 
+    add_column :programs, :active, :boolean, :default => true 
   end
 
   def self.down
