f564d92e9882ebdbfc4be6e4e83b7f61ab1bf6d6
Andrew <adharris@gmail.com>
2011-04-12 20:52:08 -0400

    fixing devise

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-SspCtab::Application.config.session_store :cookie_store, :key => '_ssp_ctab_session'
+SspCtab::Application.config.session_store :cookie_store, :key => '_ssp_ctab_session_h'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
