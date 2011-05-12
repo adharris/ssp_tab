b0047d0542655e3070239060b871926d30f3fb74
Andrew Harris <adharris@gmail.com>
2011-05-11 20:59:45 -0400

    fixed bug due to autocomplete gem update
    the newest version of the jquery autocomplete gem requires a third parameter for a function i was using.  Added nil arguments

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,18 +1,18 @@
 class ProgramsController < ApplicationController
   load_and_authorize_resource
 
-  # autocomplete :user, :name, :full => true, :scope => :current_staff
+  autocomplete :user, :name, :full => true, :scopes => [:current_staff]
 
-  def autocomplete_user_name
-    @program = Program.find(params[:program_id])
-    @users = (User.not_admin.search_by_name(params[:term]) - @program.users)
-    render :json => json_for_autocomplete(@users, :name)
-  end
+  #def autocomplete_user_name
+  #  @program = Program.find(params[:program_id])
+  #  @users = (User.not_admin.search_by_name(params[:term]) - @program.users)
+  #  render :json => json_for_autocomplete(@users, :name, nil)
+  #end
 
   def autocomplete_food_item
     @program = Program.find(params[:program_id])
     @food_items = FoodItem.all_for_program(@program).search_by_name(params[:term])
-    render :json => json_for_autocomplete(@food_items, :name)
+    render :json => json_for_autocomplete(@food_items, :name, nil)
   end
 
   def activation
